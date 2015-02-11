53770f933d6a6bd9e2a98685b37bb9084921a4be
Sam Winslow <sammywinslow@gmail.com>
2015-02-11 00:10:25 -0800

    Added seminar assets, 2 bottom sections, responsive work

--- res/js/main.js
+++ res/js/main.js
@@ -1,1 +1,27 @@
+function setHeights(){
+  if($(window).width() > 992){
+    var h = document.querySelector("#promos").offsetHeight;
+    document.querySelector("#promos .seminar-section").style.height = h + "px";
+    document.querySelector("#promos .promo-section").style.height = h + "px";
+  } else {
+    document.querySelector("#promos .seminar-section").style.height ="auto";
+    document.querySelector("#promos .promo-section").style.height = "auto";
+  }
+}
 
+$(window).resize(setHeights);
+
+$(window).scroll(function(){
+  if($(".navbar").offset().top>50){
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else{
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");}
+  });
+
+$(function(){
+  $('.page-scroll a').bind('click',function(event){
+    var $anchor=$(this);$('html, body').stop().animate({scrollTop:$($anchor.attr('href')).offset().top},1500,'jswing');
+    event.preventDefault();
+  });
+  setHeights();
+});
