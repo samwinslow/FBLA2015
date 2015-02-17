cc30c4451ec5f1e8b2153b2e0e66f7f5864408ab
Sam Winslow <sammywinslow@gmail.com>
2015-02-16 16:41:27 -0800

    Barebones signin/signout system using cookies

---
+++ mysql-admin/signin-user.php
@@ -0,0 +1,33 @@
+<?php
+
+header("Content-Type: text/plain");
+
+if (isset($_REQUEST['email']) && $_REQUEST['email'] !== '') {
+  $email = $_REQUEST['email'];
+} else {
+  die("Email not provided");
+}
+if (isset($_REQUEST['password']) && $_REQUEST['password'] !== '') {
+  $password = $_REQUEST['password'];
+} else {
+  die("Password not provided");
+}
+
+// Connect to server and select databse.
+mysql_connect("localhost", "root", "password") or die("cannot connect"); 
+mysql_select_db("cyclefitness") or die("cannot select DB");
+
+$result = mysql_query("SELECT * FROM users WHERE email_address='".$email."' and password = '".$password."';");
+mysql_close();
+
+$thirty_days = time() + 60*60*24*30;
+
+if (mysql_num_rows($result) === 1){
+  setcookie("cyclefitness_email", $email, $thirty_days, "/");
+  setcookie("cyclefitness_password", $password, $thirty_days, "/");
+  die("Signin successful");
+} else {
+  die("Signin unsuccessful");
+}
+
+?> 
---
+++ mysql-admin/signout-user.php
@@ -0,0 +1,10 @@
+<?php
+
+header("Content-Type: text/plain");
+
+setcookie("cyclefitness_email", $_COOKIE['cyclefitness_email'], time()-3600, "/");
+setcookie("cyclefitness_password", $_COOKIE['cyclefitness_password'], time()-3600, "/");
+
+die("Signout successful");
+
+?> 
