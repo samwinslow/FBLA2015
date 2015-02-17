6fe398552c589a016666320b43fd11391fe72bd5
Sam Winslow <sammywinslow@gmail.com>
2015-02-16 17:14:52 -0800

    Added administrative login layer

--- mysql-admin/signin-user.php
+++ mysql-admin/signin-user.php
@@ -13,6 +13,13 @@ if (isset($_REQUEST['password']) && $_REQUEST['password'] !== '') {
   die("Password not provided");
 }
 
+if ($email === 'admin' && $password === 'password'){
+  setcookie("cyclefitness_email", $email, $thirty_days, "/");
+  setcookie("cyclefitness_password", $password, $thirty_days, "/");
+  setcookie("cyclefitness_admin", "true", $thirty_days, "/");
+  die("Administrative signin successful");
+}
+
 // Connect to server and select databse.
 mysql_connect("localhost", "root", "password") or die("cannot connect"); 
 mysql_select_db("cyclefitness") or die("cannot select DB");
@@ -25,6 +32,7 @@ $thirty_days = time() + 60*60*24*30;
 if (mysql_num_rows($result) === 1){
   setcookie("cyclefitness_email", $email, $thirty_days, "/");
   setcookie("cyclefitness_password", $password, $thirty_days, "/");
+  setcookie("cyclefitness_admin", "false", $thirty_days, "/");
   die("Signin successful");
 } else {
   die("Signin unsuccessful");
