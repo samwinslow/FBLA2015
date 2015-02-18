745fc1e59f4cf571cabe85e57a113986e0a78ff7
Sam Winslow <sammywinslow@gmail.com>
2015-02-17 23:45:59 -0800

    Added seminar page content

--- seminars.php
+++ seminars.php
@@ -155,77 +155,77 @@ if (isset($_GET['error'])){
   <div class="container seminar-container">   
     <div class="row">
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
+        <img class="img-circle" src="res/img/seminar-pg-1.png">
+        <h3>Staying safe on the road: Bike Safety 101</h3>
+        <p>This intro seminar goes over the basics of staying safe on the road.</p>
+        <a class="caps" href="#">Watch Seminar</a>
+      </div>
+      <div class="col-md-2 seminar-tile seminar-page">
+        <img class="img-circle" src="res/img/seminar-pg-2.png">
+        <h3>Bicycle Types: How to Pick the Best Bike for You</h3>
+        <p>This seminar goes over the functions of different types of bikes.</p>
+        <a class="caps" href="#">Watch Seminar</a>
+      </div>
+      <div class="col-md-2 seminar-tile seminar-page">
+        <img class="img-circle" src="res/img/seminar-pg-3.png">
         <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
         <p>This seminar helps get you started with cycling for fitness.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-4.png">
+        <h3>Bike Identification: Identifying basic bike parts</h3>
+        <p>This seminar goes over how to identify the basic parts of a bike.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-5.png">
+        <h3>Bike maintenance: How to tune a bike</h3>
+        <p>This seminar goes over topics like clearning and adjustments for bikes.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
-        <a class="caps" href="#">Watch Seminar</a>
-      </div>
-      <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
-        <a class="caps" href="#">Watch Seminar</a>
-      </div>
-      <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-6.png">
+        <h3>Bike Installation: Installing a new Derailleur</h3>
+        <p>This seminar is a step by step instruction on installation.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
     </div>
     <div class="row">
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-7.png">
+        <h3>Packaging: How to safely package your bike</h3>
+        <p>This intro seminar goes over the deconstruction and packaging of a bike.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-8.png">
+        <h3>Bike Riding: How to shift your gears</h3>
+        <p>This seminar helps you shift gears on different types of bikes.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-9.png">
+        <h3>Cycling Trips: How to plan a good route</h3>
+        <p>This seminar goes over the basics of planning a good route.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-10.png">
+        <h3>Cycling Trips: Packing essentials for trips</h3>
+        <p>This seminar goes over the essentials of a cycling trip.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-11.png">
+        <h3>Riding: How to ensue a repair-free ride</h3>
+        <p>This seminar shows you how to prevent bike malfunctions on the road.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
       <div class="col-md-2 seminar-tile seminar-page">
-        <img class="img-circle" src="res/img/seminar-3.png">
-        <h3>Cycling for fitness: Bike &amp; Cycling Exercises</h3>
-        <p>This seminar helps get you started with cycling for fitness.</p>
+        <img class="img-circle" src="res/img/seminar-pg-12.png">
+        <h3>Family Biking 101: Basics of group biking</h3>
+        <p>This seminar goes over the basics of group biking.</p>
         <a class="caps" href="#">Watch Seminar</a>
       </div>
     </div>
